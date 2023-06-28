5fe0a795a519adfa83d5f2df38629085b228b7a6
lauren haas <laurenraehaas@gmail.com>
2023-06-28 10:54:06 -0500

    adding questions into java

---
+++ js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "1. Creative Style Sheets",
+            "2. Cascading Style Sheets",
+            "3. Computer Style Sheets",
+            "4. Colorful Style Sheets"
+        ],
+        answer: "2. Cascading Style Sheets"
+    },
+    {
+        question: "Which HTML tag is used to link an external CSS file?",
+        options: [
+            "1. link",
+            "2. style",
+            "3. css",
+            "4. script"
+        ],
+        answer: "1. link"
+    },
+    {
+        question: "Which event is triggered when a user clicks on an HTML element?",
+        options: [
+            "1. onhover",
+            "2. ondrag",
+            "3. onclick",
+            "4. onsubmit"
+        ],
+        answer: "3. onclick"
+    },
+    {
+        question: "Which JavaScript function is used to change the content of an HTML element?",
+        options: [
+            "1. getElementById()",
+            "2. document.write()",
+            "3. innerHTML()",
+            "4. changeContent()"
+        ],
+        answer: "3. innerHTML()"
+    },
+    {
+        question: "What is the purpose of the 'querySelector' method in JavaScript?",
+        options: [
+            "1. To select an HTML element based on its class name",
+            "2. To select an HTML element based on its ID",
+            "3. To select an HTML element based on its tag name",
+            "4. All of the above"
+        ],
+        answer: "4. All of the above"
+    },
+]
